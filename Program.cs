dccf4a1e364c760227ed5a5393255c1a01cc1576
julialoiusek <kasperssonjulia@gmail.com>
2025-10-01 13:38:49 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ConsoleLottery
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
