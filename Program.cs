68f4bc8230ca29d96eb798f55dfea5f036730462
loiikoii <vappelquist03@gmail.com>
2025-10-01 15:50:42 +0200

    Added choice menu

--- Program.cs
+++ Program.cs
@@ -4,7 +4,66 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int[] col = Buy();
+            foreach (var i in col)
+            {
+                Console.WriteLine(i);
+            }
+
         }
+
+        //static int[] Buy()
+        //{
+        //    int lottery = 0;
+        //    int userNumber = -1;
+        //    int[] col = new int[5];
+
+        //    while (true)
+        //    {
+
+        //        Console.Write("Skriv en siffra mellan 1-50: ");
+
+
+        //        while (!int.TryParse(Console.ReadLine(), out userNumber) && userNumber <= 1 && userNumber >= 50)
+        //        {
+        //            Console.WriteLine("Det måste vara en siffra mellan 1-50");
+        //        }
+
+        //        col[lottery] = userNumber;
+        //        if (lottery != 4)
+        //        {
+        //            lottery++;
+        //        }
+        //        else if (lottery == 4)
+        //        {
+        //            return col;
+        //        }
+        //    }
+
+        //}
+        static int[] Buy()
+        {
+            int lottery = 0;
+            int userNumber = -1;
+            int[] col = new int[5];
+            int remaining = 5;
+            while (remaining != 0)
+            {
+                Console.Write("Skriv en siffra mellan 1-50: ");
+                while (!int.TryParse(Console.ReadLine(), out userNumber) || userNumber <= 0 || userNumber >= 51)
+                {
+                    Console.WriteLine("Det måste vara en siffra mellan 1-50");
+                }
+                col[lottery] = userNumber;
+                lottery++;
+                remaining--;
+                
+            }
+            return col;
+        }
+
+
     }
+
 }
+
